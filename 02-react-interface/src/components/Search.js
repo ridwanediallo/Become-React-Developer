c3d7b109aee465e4fdd9fff83b34cbe512773964
Ridwan <ridwanediallo99@gmail.com>
2022-03-26 15:38:09 +0000

    Add toggleSort

--- 02-react-interface/src/components/Search.js
+++ 02-react-interface/src/components/Search.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import {BiSearch, BiCaretDown, BiCheck} from "react-icons/bi";
 
-const DropDown = () => {
+const DropDown = ({toggle}) => {
+  if(!toggle) {
+    return null;
+  }
     return (
         <div className="origin-top-right absolute right-0 mt-2 w-56
       rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
@@ -28,6 +32,7 @@ const DropDown = () => {
 
 
 function Search() {
+  const [toggleSort, setToggleSort] = useState(false);
   return (
     <div className="py-5">
     <div className="mt-1 relative rounded-md shadow-sm">
@@ -39,11 +44,11 @@ function Search() {
         className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
       <div className="absolute inset-y-0 right-0 flex items-center">
         <div>
-          <button type="button"
+          <button type="button" onClick={() => {setToggleSort(!toggleSort)}}
             className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
             Sort By <BiCaretDown className="ml-2" />
           </button>
-          <DropDown />
+          <DropDown toggle={toggleSort}/>
         </div>
       </div>
     </div>
