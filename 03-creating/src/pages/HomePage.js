e2c2617dff2fd9facd70cd22e8b90f4640a790d6
Ridwan <ridwanediallo99@gmail.com>
2022-03-27 21:28:18 +0000

    Add HomePage component and react router dom setup

---
+++ 03-creating/src/pages/HomePage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <>
+      <h1>Hello welcome to my blog</h1>
+      <p>
+        Welcome to my blog! Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Magnam dolor illo et ex ea ducimus velit voluptatem, eaque
+        repellendus earum magni esse distinctio veniam ratione fugiat quas
+        praesentium? Officiis, amet. Beatae cumque repellat perferendis
+        consequuntur animi facilis eos sint ullam possimus dolorum. Ad, voluptas
+        quos enim veritatis unde consequatur, quibusdam culpa quae at atque
+        consectetur, rem id corrupti reiciendis commodi. Architecto recusandae
+        modi, iure aliquam iste soluta ea id blanditiis minima nesciunt error
+        at, alias quod vel fuga odit dolores sit qui non omnis fugiat,
+        reiciendis quia. Nesciunt, facere eius.
+      </p>
+      <p>
+        Welcome to my blog! Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Magnam dolor illo et ex ea ducimus velit voluptatem, eaque
+        repellendus earum magni esse distinctio veniam ratione fugiat quas
+        praesentium? Officiis, amet. Beatae cumque repellat perferendis
+        consequuntur animi facilis eos sint ullam possimus dolorum. Ad, voluptas
+        quos enim veritatis unde consequatur, quibusdam culpa quae at atque
+        consectetur, rem id corrupti reiciendis commodi. Architecto recusandae
+        modi, iure aliquam iste soluta ea id blanditiis minima nesciunt error
+        at, alias quod vel fuga odit dolores sit qui non omnis fugiat,
+        reiciendis quia. Nesciunt, facere eius.
+      </p>
+    </>
+  );
+}
+
+export default HomePage;
