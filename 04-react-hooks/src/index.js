8323db1fef6dc441cb2ba451e599a748fa0b7f6e
Ridwan <ridwanediallo99@gmail.com>
2022-03-30 13:25:47 +0000

    Add an example with useReducer

--- 04-react-hooks/src/index.js
+++ 04-react-hooks/src/index.js
@@ -1,32 +1,37 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 
+const initialState = {
+  message: 'Hello'
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'yell':
+      return {
+        message: `HEY! I just said ${state.message}`
+      };
+    case 'whisper':
+      return {
+        message: `excuse me I just said ${state.message}`
+      };
+  }
+
+}
 
 function App() {
-  const [name, setName] = useState('Ridwan');
-  const [data, setData] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-
-  useEffect(() => {
-    fetch('https://api.github.com/users')
-    .then((response) => response.json())
-    .then(setData)
-  }, [])
-
-  if (data) {
     return (
       <>
-        <ul>
-          {data.map((data) => (
-            <li key={data.id}>{data.login}</li>
-          ))}
-        </ul>
-        <button onClick={() => setData([])}>Remove data</button>
+       <p>Message: {state.message}</p>
+       <button onClick={() => dispatch({type: 'yell'})}>Hello</button>
+       <button onClick={() => dispatch({type: 'whisper'})}>whisper</button>
       </>
     );
-  }
+
 }
 
 ReactDOM.render(
@@ -35,6 +40,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+
 
 
 
