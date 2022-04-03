3d7d804214f9297aa905df5cc1b35a72d62cee62
Ridwan <ridwanediallo99@gmail.com>
2022-04-03 15:41:27 +0000

    Connect components to the store

--- 06-react-ecosysteme/src/todos/NewTodoForm.js
+++ 06-react-ecosysteme/src/todos/NewTodoForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import {connect} from 'react-redux';
+import { createTodo } from './Actions';
 import './NewTodoForm.css';
 
-export default function NewTodoForm() {
+function NewTodoForm({ todos, onCreatePressed }) {
   const [inputValue, setInputValue] = useState('');
   return (
     <div className="new-todo-form">
@@ -12,9 +14,28 @@ export default function NewTodoForm() {
         onChange={(e) => setInputValue(e.target.value)}
         placeholder="Type your new todo here"
       />
-      <div className="new-todo-button">Create Todo</div>
+      <button onClick={() => {
+        const isDuplicateText = todos.some(todo => todo.text === inputValue);
+          if(!isDuplicateText) {
+            onCreatePressed(inputValue);
+            setInputValue('');
+          }
+      }}
+      className="new-todo-button">
+        Create Todo
+      </button>
     </div>
   );
 }
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onCreatePressed: text => dispatch(createTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
 
 
